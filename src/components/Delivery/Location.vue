65b9fcebac42e97a287d759157b0f6a9fad7b978
annielin-linkage <annie.lin@linkage-retail.com>
2022-07-07 15:46:20 +0800

    Add Outstanding  location

--- src/components/Delivery/Location.vue
+++ src/components/Delivery/Location.vue
@@ -1,1 +1,30 @@
-<template>Location</template>
+<template>
+  <section class="text-weight-light text-body2 text-grey-8 q-pa-lg q-gutter-y-md">
+    <!-- Name -->
+    <div class="q-gutter-y-xs">
+      <div>Name :</div>
+      <div class="text-blue-4 text-weight-regular">Big Big Cafe</div>
+    </div>
+    <!-- Contact -->
+    <div class="q-gutter-y-xs">
+      <div>Contact :</div>
+      <div class="text-blue-4 text-weight-regular">9666 7766</div>
+    </div>
+    <!-- Address -->
+    <div class="q-gutter-y-xs">
+      <div>Address :</div>
+      <div class="text-blue-4 text-weight-regular">新界元朗大馬路100號</div>
+    </div>
+    <q-separator color="grey-3" />
+    <!-- Deliveried Time -->
+    <div class="q-gutter-y-xs">
+      <div>Deliveried Time :</div>
+      <div class="text-blue-4 text-weight-regular">-</div>
+    </div>
+    <!-- Handled by -->
+    <div class="q-gutter-y-xs">
+      <div>Handled by :</div>
+      <div class="text-blue-4 text-weight-regular">-</div>
+    </div>
+  </section>
+</template>
